5c07042ea3e2004c2015a1b87d04cbbde0ab5702
Susan <susansewe@gmail.com>
2024-04-24 12:53:06 -0700

    All Tasks

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#using puppet to connect without password
+file { '/etc/ssh/ssh_config':
+  ensure => present,
+
+}
+
+file_line { 'Turn off password auth':
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no',
+  match => '^#PasswordAuthentication',
+
+}
+
+file_line { 'Declare identity file':
+  path => '/etc/ssh/ssh_config'
+  line => 'IdentiftyFile ~/.ssh/school',
+  match => '^#IdentityFile',
+
+}
+
